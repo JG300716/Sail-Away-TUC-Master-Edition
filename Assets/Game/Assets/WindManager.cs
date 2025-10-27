fc9a15176d950c66b8f52ad01d9f427d3d1f7122
JayCoub <91854307+JG300716@users.noreply.github.com>
2025-10-27 20:26:48 +0100

    Basic wind manager

---
+++ Assets/Game/Assets/WindManager.cs
@@ -0,0 +1,20 @@
+﻿namespace Game.Assets
+{
+    public class WindManager
+    {
+        // --- Warunki wiatru rzeczywistego ---
+        private double V_true; // prędkość rzeczywistego wiatru [m/s]
+        private double BetaTrueDeg; // kąt rzeczywistego wiatru względem dziobu [°]
+        
+        
+        public double getWindSpeed()
+        {
+            return V_true;
+        }
+        
+        public double getWindDirection()
+        {
+            return BetaTrueDeg;
+        }
+    }
+}
