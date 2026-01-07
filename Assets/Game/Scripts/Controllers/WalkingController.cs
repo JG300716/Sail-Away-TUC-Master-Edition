3c873fcef460fd961edadd66180f6a127ae3cb64
JayCoub <91854307+JG300716@users.noreply.github.com>
2026-01-07 21:35:06 +0100

    Fix 2

--- Assets/Game/Scripts/Controllers/WalkingController.cs
+++ Assets/Game/Scripts/Controllers/WalkingController.cs
@@ -48,6 +48,7 @@ namespace Game.Scripts.Controllers
             if (!isActive) return;
             if (!triggerBuffor) return;
             triggerBuffor = false;
+            Debug.Log($"LOG {other.name} {other.tag}");
             if (other.IsUnityNull()) return;
             if (other.CompareTag("Enter")) GameManager.EnterYacht();
             if (other.CompareTag("Exit")) GameManager.LeaveYacht();
